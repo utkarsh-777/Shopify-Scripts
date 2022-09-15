e6e9b833a6184929a2936368c4b3d44abcb22a8f
Utkarsh Kashyap <kumarutkarsh305@gmail.com>
2022-09-15 14:48:10 +0530

    added injectCSS func

--- myScript.js
+++ myScript.js
@@ -1,25 +1,38 @@
 const onProductPage = window.location.href.includes("products");
+const cssLinks = [
+  "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css",
+  "https://cdn.jsdelivr.net/gh/utkarsh-777/Shopify-Scripts/test.css",
+];
 
-function main() {
-  if (onProductPage) {
-    const bellElementDiv = document.createElement("div");
-    bellElementDiv.setAttribute("class", "bellIcon");
+const injectCSS = () => {
+  cssLinks.forEach((cssLink) => {
+    let link = document.createElement("link");
+    link.href = cssLink;
+    link.type = "text/css";
+    link.rel = "stylesheet";
+    document.getElementsByTagName("head")[0].appendChild(link);
+  });
+};
 
-    const productForm = document.getElementsByClassName("product-form")[0];
-    if (!productForm) {
-      return;
-    }
+const main = () => {
+  const bellElementDiv = document.createElement("div");
+  bellElementDiv.setAttribute("class", "bellIcon");
 
-    const icon = document.createElement("i");
-    icon.setAttribute("class", "bi bi-bell-fill");
-    bellElementDiv.appendChild(icon);
-
-    icon.addEventListener("click", function () {
-      alert("bell clicked!");
-    });
-
-    productForm.appendChild(bellElementDiv);
+  const productForm = document.getElementsByClassName("product-form")[0];
+  if (!productForm) {
+    return;
   }
-}
+
+  injectCSS();
+  const icon = document.createElement("i");
+  icon.setAttribute("class", "bi bi-bell-fill");
+  bellElementDiv.appendChild(icon);
+
+  icon.addEventListener("click", function () {
+    alert("bell clicked!");
+  });
+
+  productForm.appendChild(bellElementDiv);
+};
 
 main();
