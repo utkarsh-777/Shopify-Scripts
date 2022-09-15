8516362743c82fa2c84ee84c8d6da2927823be18
Utkarsh Kashyap <kumarutkarsh305@gmail.com>
2022-09-15 14:34:55 +0530

    added css and test html

--- myScript.js
+++ myScript.js
@@ -1,1 +1,25 @@
-console.log("JS INJECTED SUCCESSFULLY!");
+const onProductPage = window.location.href.includes("products");
+
+function main() {
+  if (onProductPage) {
+    const bellElementDiv = document.createElement("div");
+    bellElementDiv.setAttribute("class", "bellIcon");
+
+    const productForm = document.getElementsByClassName("product-form")[0];
+    if (!productForm) {
+      return;
+    }
+
+    const icon = document.createElement("i");
+    icon.setAttribute("class", "bi bi-bell-fill");
+    bellElementDiv.appendChild(icon);
+
+    icon.addEventListener("click", function () {
+      alert("bell clicked!");
+    });
+
+    productForm.appendChild(bellElementDiv);
+  }
+}
+
+main();
